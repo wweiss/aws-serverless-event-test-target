5343f3134e0ac2ff167b5119fcbcf18b4790499a
William Weiss <william@codification.org>
2021-08-31 14:27:54 -0700

    chore: upgrade to new build process

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,38 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 const path = require("path");
-const webpackTask = require('@codification/cutwater-build-webpack').webpack;
-const isProduction = webpackTask.buildConfig.production;
+const webpack = require('webpack');
+const buildConfig = require('@codification/cutwater-build-web').getConfig();
+const isProduction = buildConfig.production;
 const webpackConfiguration = {
     mode: isProduction ? 'production' : 'development',
     entry: {
-        'testTarget':  path.join(__dirname, webpackTask.buildConfig.libFolder, 'EventTestTargetHandler.js')
+        'index':  path.join(__dirname, buildConfig.srcFolder, 'EventTestTargetHandler.ts')
     },
     output: {
         libraryTarget: 'umd',
-        path: path.join(__dirname, webpackTask.buildConfig.distFolder),
+        path: path.join(__dirname, buildConfig.distFolder),
         filename: `[name].js`,
         sourceMapFilename: "[name].js.map"
     },
-    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    devtool: isProduction ? undefined : 'inline-source-map',
+    optimization: {
+        minimize: false,
+    },
+    target: 'node',
     externals: ['aws-sdk'],
-    target: 'node'
+    plugins: [new webpack.IgnorePlugin(/^electron$/)]
 };
 module.exports = webpackConfiguration;
